472156dcfe8e0e46e9dbc3fdf01575fd3ac31cfd
pathtofile <git@tofile.dev>
2022-08-22 11:28:12 +1000

    try to fix link

--- Casks/toucli.rb
+++ Casks/toucli.rb
@@ -1,8 +1,8 @@
 cask "toucli" do
-    version "1.0"
-    sha256 "9f544ce9d9f3100054745377759303213744b5ed9e7940d956efed3724696868"
+    version :latest
+    sha256 :no_check
   
-    url "https://github.com/pathtofile/toucli/releases/download/v#{version}/toucli.zip"
+    url "https://github.com/pathtofile/toucli/releases/latest/download/toucli.zip"
     name "toucli"
     desc "Use touchID and the Secure Enclave to encrypt data from the commandline"
     homepage "https://github.com/pathtofile/toucli"
